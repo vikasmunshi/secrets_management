16fad0498b469a28e6ca317b31b487b5b8af10ae
Vikas Munshi <vikas.munshi@ing.com>
2018-08-30 23:25:16 +0200

    updated version format to major.minor.build

--- version.py
+++ version.py
@@ -5,9 +5,11 @@
 from os import path
 from time import time
 
-base_dirname = path.dirname(__file__)
+base_dirname = path.dirname(path.abspath(__file__))
+major_version = 0
+minor_version = 2
 package_name = 'cloak'
-package_version = '0.2.{}'.format(int(time()) // 3600)
+package_version = '{}.{}.{}'.format(major_version, minor_version, int(time()) // 3600)
 
 
 def update_egg(filename: str) -> None:
@@ -25,10 +27,10 @@ def update_egg(filename: str) -> None:
         outfile.writelines(file_contents)
 
 
-def update_version(filename: str, version_line_identifier: str) -> None:
-    version_line = '{} = \'{}\'\n'.format(version_line_identifier, package_version)
+def update_version(filename: str, var_name: str) -> None:
+    version_line = '{} = \'{}\'\n'.format(var_name, package_version)
     with open(filename) as infile:
-        file_contents = [version_line if l.startswith(version_line_identifier) else l for l in infile.readlines()]
+        file_contents = [version_line if l.startswith(var_name) else l for l in infile.readlines()]
     with open(filename, 'w') as outfile:
         outfile.writelines(file_contents)
 
@@ -37,8 +39,8 @@ if __name__ == '__main__':
     for file_with_egg in (path.join(base_dirname, 'Dockerfile'), path.join(base_dirname, 'README.md')):
         update_egg(file_with_egg)
 
-    for file_with_version, identifier_text in (
+    for file_with_version, version_var_name in (
             (path.join(base_dirname, package_name, '__init__.py'), '__version__'),
             (path.join(base_dirname, package_name, 'tests', '__init__.py'), '__version__'),
             (path.join(base_dirname, 'setup.py'), 'package_version')):
-        update_version(file_with_version, identifier_text)
+        update_version(file_with_version, version_var_name)
