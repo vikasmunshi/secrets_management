2a58ef76c1de54a317272c8d48ed3ee4f6672243
Vikas Munshi <vikas.munshi@ing.com>
2018-09-23 00:22:26 +0200

    Reverted use of find_packages()

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 from setuptools import setup
 
 package_name = 'cloak'
-package_version = '0.3.427126'
+package_version = '0.3.4271263'
 
 with open(path.join(path.dirname(__file__), 'requirements.txt')) as rf:
     package_requirements = rf.readlines()
