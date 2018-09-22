7d8da42b42e0c3d4ec78ff18f895736723d6d964
Vikas Munshi <vikas.munshi@ing.com>
2018-09-23 00:25:02 +0200

    minor version number updated

--- cloak/tests/__init__.py
+++ cloak/tests/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.4271263'
+__version__ = '0.3.427126'
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 
 from os import path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'cloak'
-package_version = '0.3.4271263'
+package_version = '0.3.427126'
 
 with open(path.join(path.dirname(__file__), 'requirements.txt')) as rf:
     package_requirements = rf.readlines()
@@ -28,7 +28,7 @@ setup(
     long_description=open('README.md').read(),
     name='{}-{}'.format(package_name, package_version),
     package_dir={package_name: package_name},
-    packages=[package_name],
+    packages=find_packages(),
     platforms=['Linux', 'MacOS'],
     python_requires='>=3.7',
     url='https://github.com/vikasmunshi/secrets_management/',
